bfbc165f189f8a17266c108aae13b2952a0979e7
zakarialaalyej <163797519+zakarialaalyej@users.noreply.github.com>
2024-05-20 23:58:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var users = [
+    { Email: 'ziko@gmail.com', password: 'zaki123456', Firstname: 'ziko', Secondname: 'bobich' }, 
+    { Email: 'zakaria@gmail.com', password: '12345abc', Firstname: 'zakaria', Secondname: 'laalyej' }
+]; 
